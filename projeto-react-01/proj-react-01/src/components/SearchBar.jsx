170cf0b6994a0f4f7ef576949de32767dee11749
Halina Amorim <halinaamorim@gmail.com>
2026-02-10 20:22:00 -0300

    adiciona proj react atualizado: Consumo de API e Rotas

--- projeto-react-01/proj-react-01/src/components/SearchBar.jsx
+++ projeto-react-01/proj-react-01/src/components/SearchBar.jsx
@@ -1,28 +1,16 @@
-import { useState } from "react";
-import { BsSearch } from "react-icons/bs";
 import "./SearchBar.css";
 
 const SearchBar = ({ handleSearch }) => {
-  const [query, setQuery] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleSearch(query);
-  };
-
-  return (
-    <form className='search-container' onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder='Pesquise por um modelo de carro...'
-        value={query || ""}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button type='submit'>
-        <BsSearch />
-      </button>
-    </form>
-  );
+    return (
+        <div className="search-container">
+            <input 
+                type="text" 
+                placeholder="Pesquise por modelo, marca ou título..."
+                onChange={(e) => handleSearch(e.target.value)} 
+            />
+            
+        </div>
+    );
 };
 
 export default SearchBar;
